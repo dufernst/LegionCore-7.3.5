78bd83d5f01b2687f8e584a41bf5e60eca2c9e8c
dufernst <dufernst@epicduf.nl>
2021-01-29 19:50:13 +0100

    Lock the allied races behind their respective unlock achievements.

---
+++ sql/updates/world/0002_allied_races_achievement_unlock.sql
@@ -0,0 +1,4 @@
+UPDATE race_unlock_requirement SET expansion=6, achievementId=12244 WHERE raceID=27;
+UPDATE race_unlock_requirement SET expansion=6, achievementId=12245 WHERE raceID=28;
+UPDATE race_unlock_requirement SET expansion=6, achievementId=12242 WHERE raceID=29;
+UPDATE race_unlock_requirement SET expansion=6, achievementId=12243 WHERE raceID=30;
