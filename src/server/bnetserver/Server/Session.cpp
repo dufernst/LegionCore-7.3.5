a3e6634220c3702bcf6cf9e85d59410ccdc43e59
dufernst <dufernst@epicduf.nl>
2021-12-28 21:51:15 +0100

    Correct identity check during the realm list fetching process (fixed by @zpfzzz).

--- src/server/bnetserver/Server/Session.cpp
+++ src/server/bnetserver/Server/Session.cpp
@@ -434,7 +434,7 @@ uint32 Battlenet::Session::GetRealmListTicket(std::unordered_map<std::string, Va
         ::JSON::RealmList::RealmListTicketIdentity data;
         std::size_t jsonStart = identity->blob_value().find(':');
         if (jsonStart != std::string::npos && ::JSON::Deserialize(identity->blob_value().substr(jsonStart + 1), &data))
-            if (_accountInfo->Id == data.gameaccountid());
+            if (_accountInfo->Id == data.gameaccountid())
                 validIdentity = true;
     }
 
