b54f1a9e0f56f6ad5f37808d94940a04b657b3e6
yudian1944 <34819144+yudian1944@users.noreply.github.com>
2022-08-23 10:09:52 -0400

    Update smart_script for npc 105769.sql

--- sql/updates/world/smart_script for npc 105769.sql
+++ sql/updates/world/smart_script for npc 105769.sql
@@ -2,7 +2,3 @@
 DELETE FROM `smart_scripts` WHERE `entryorguid` = 105769;
 insert into `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) values('105769','0','0','0','62','0','100','0','19521','0','0','0','85','212023','0','0','0','0','0','7','0','0','0','0','0','0','0','Teleport to Dalaran');
 UPDATE `creature_template` SET `AIName` = 'SmartAI' WHERE `entry` = 105769; 
-
--- añadiendo spawn del npc 105586 necesario para la mision 41966
-DELETE FROM `creature` WHERE `guid` = 280949079;
-insert into `creature` (`guid`, `id`, `map`, `zoneId`, `areaId`, `spawnMask`, `phaseMask`, `PhaseId`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `MovementType`, `npcflag`, `npcflag2`, `unit_flags`, `dynamicflags`, `AiID`, `MovementID`, `MeleeID`, `isActive`, `skipClone`, `personal_size`, `isTeemingSpawn`, `unit_flags3`) values('280949079','105586','1220','7502','7596','1','65535','','0','0','-783.715','4577.37','728.747','3.80133','300','0','0','1039267','0','0','0','0','0','0','0','0','0','0','0','0','0','0');
