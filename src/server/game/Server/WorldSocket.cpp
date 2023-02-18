5df30e03925b609bb01fc59ca476c7468b2e1032
dufernst <dufernst@epicduf.nl>
2023-02-18 13:16:30 +0100

    Fix unique player check on first login.
    Previous solution does not work because the last_ip value gets updated before hitting the WorldSocket.

--- src/server/game/Server/WorldSocket.cpp
+++ src/server/game/Server/WorldSocket.cpp
@@ -647,6 +647,7 @@ struct AccountInfo
     {
         bool IsLockedToIP;
         std::string LastIP;
+        std::string FirstIP;
         std::string LockCountry;
         LocaleConstant Locale;
 
@@ -675,28 +676,29 @@ struct AccountInfo
 
     explicit AccountInfo(Field* fields)
     {
-        //          0          1            2          3               4           5           6          7           8     9           10          11         12
-        // SELECT a.id, a.sessionkey, a.last_ip, a.locked, a.lock_country, a.expansion, a.mutetime, a.locale, a.recruiter, a.os, aa.gmLevel, a.AtAuthFlag, a.referer
-        //                                                           13   14       15
+        //          0          1            2          3          4           5           6          7           8          9           10          11         12        13
+        // SELECT a.id, a.sessionkey, a.last_ip, a.first_ip, a.locked, a.lock_country, a.expansion, a.mutetime, a.locale, a.recruiter, a.os, aa.gmLevel, a.AtAuthFlag, a.referer
+        //                                                           14   15       16
         // ab.unbandate > UNIX_TIMESTAMP() OR ab.unbandate = ab.bandate, r.id, a.hwid
         // FROM account a LEFT JOIN account r ON a.id = r.recruiter LEFT JOIN account_access aa ON a.id = aa.id AND aa.RealmID IN (-1, ?) LEFT JOIN account_banned ab ON a.id = ab.id AND ab.active = 1
         // WHERE a.username = ? ORDER BY aa.RealmID DESC LIMIT 1
         Game.Id = fields[0].GetUInt32();
         HexStrToByteArray(fields[1].GetString(), Game.KeyData.data());
         BattleNet.LastIP = fields[2].GetString();
-        BattleNet.IsLockedToIP = fields[3].GetBool();
-        BattleNet.LockCountry = fields[4].GetString();
-        Game.Expansion = /*std::max(fields[5].GetUInt8(), */CURRENT_EXPANSION;//);
-        Game.MuteTime = fields[6].GetInt64();
-        BattleNet.Locale = LocaleConstant(fields[7].GetUInt8());
-        Game.Recruiter = fields[8].GetUInt32();
-        Game.OS = fields[9].GetString();
-        Game.Security = AccountTypes(fields[10].GetUInt8());
-        Game.IsBanned = fields[13].GetUInt64() != 0;
-        Game.IsRectuiter = fields[14].GetUInt32() != 0;
-        Game.AtAuthFlag = AuthFlags(fields[11].GetUInt16());
-        Game.Referer = fields[12].GetUInt32();
-        Game.Hwid = fields[15].GetUInt64();
+        BattleNet.FirstIP = fields[3].GetString();
+        BattleNet.IsLockedToIP = fields[4].GetBool();
+        BattleNet.LockCountry = fields[5].GetString();
+        Game.Expansion = /*std::max(fields[6].GetUInt8(), */CURRENT_EXPANSION;//);
+        Game.MuteTime = fields[7].GetInt64();
+        BattleNet.Locale = LocaleConstant(fields[8].GetUInt8());
+        Game.Recruiter = fields[9].GetUInt32();
+        Game.OS = fields[10].GetString();
+        Game.Security = AccountTypes(fields[11].GetUInt8());
+        Game.IsBanned = fields[14].GetUInt64() != 0;
+        Game.IsRectuiter = fields[15].GetUInt32() != 0;
+        Game.AtAuthFlag = AuthFlags(fields[12].GetUInt16());
+        Game.Referer = fields[13].GetUInt32();
+        Game.Hwid = fields[16].GetUInt64();
         if (BattleNet.Locale >= MAX_LOCALES)
             BattleNet.Locale = LOCALE_enUS;
     }
@@ -863,7 +865,7 @@ void WorldSocket::HandleAuthSessionCallback(std::shared_ptr<WorldPackets::Auth::
     }
 
     // if first login
-    if (account.BattleNet.LastIP == "127.0.0.1")
+    if (account.BattleNet.FirstIP == "127.0.0.1")
     {
         if (sWorld->getIntConfig(CONFIG_UNIQUE_IP_TOKEN_AMOUNT) > 0)
         {
