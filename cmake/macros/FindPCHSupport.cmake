c4b08c1e58cc5e93a074018989df0e00c8a3d4f3
welder1976 <zemetskia@gmail.com>
2023-05-21 12:05:27 +0300

    check fix

--- cmake/macros/FindPCHSupport.cmake
+++ cmake/macros/FindPCHSupport.cmake
@@ -1,3 +1,13 @@
+if (MSVC)
+  # Specify the maximum PreCompiled Header memory allocation limit
+  # Fixes a compiler-problem when using PCH - the /Ym flag is adjusted by the compiler in MSVC2012,
+  # hence we need to set an upper limit with /Zm to avoid discrepancies)
+  # (And yes, this is a verified, unresolved bug with MSVC... *sigh*)
+  #
+  # Note: This workaround was verified to be required on MSVC 2017 as well
+  set(COTIRE_PCH_MEMORY_SCALING_FACTOR 500)
+endif()
+
 include(cotire)
 
 function(ADD_CXX_PCH TARGET_NAME_LIST PCH_HEADER)
@@ -10,7 +20,7 @@ function(ADD_CXX_PCH TARGET_NAME_LIST PCH_HEADER)
     set_target_properties(${TARGET_NAME} PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT ${PCH_HEADER})
 
     # Workaround for cotire bug: https://github.com/sakra/cotire/issues/138
-    set_property(TARGET ${TARGET_NAME} PROPERTY CXX_STANDARD 14)
+    set_property(TARGET ${TARGET_NAME} PROPERTY CXX_STANDARD 17)
   endforeach()
 
   cotire(${TARGET_NAME_LIST})
