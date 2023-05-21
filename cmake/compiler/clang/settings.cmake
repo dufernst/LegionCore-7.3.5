56d38f477bafa4f6afc9e3d9a5b2e95b4a33ad5d
welder1976 <zemetskia@gmail.com>
2023-05-21 11:38:24 +0300

    включаем поддержку С++14

--- cmake/compiler/clang/settings.cmake
+++ cmake/compiler/clang/settings.cmake
@@ -1,21 +1,38 @@
 # Set build-directive (used in core to tell which buildtype we used)
-add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
+target_compile_definitions(trinity-compile-option-interface
+  INTERFACE
+    -D_BUILD_DIRECTIVE="${CMAKE_BUILD_TYPE}")
 
 if(WITH_WARNINGS)
-  set(WARNING_FLAGS "-W -Wall -Wextra -Winit-self -Wfatal-errors -Wno-mismatched-tags")
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${WARNING_FLAGS}")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${WARNING_FLAGS} -Woverloaded-virtual")
+  target_compile_options(trinity-warning-interface
+    INTERFACE
+      -W
+      -Wall
+      -Wextra
+      -Winit-self
+      -Wfatal-errors
+      -Wno-mismatched-tags
+      -Woverloaded-virtual)
+
   message(STATUS "Clang: All warnings enabled")
 endif()
 
 if(WITH_COREDEBUG)
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g3")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g3")
+  target_compile_options(trinity-compile-option-interface
+    INTERFACE
+      -g3)
+
   message(STATUS "Clang: Debug-flags set (-g3)")
 endif()
 
 # -Wno-narrowing needed to suppress a warning in g3d
 # -Wno-deprecated-register is needed to suppress 185 gsoap warnings on Unix systems.
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++17 -Wno-narrowing -Wno-deprecated-register")
-set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -DDEBUG=1")
+target_compile_options(trinity-compile-option-interface
+  INTERFACE
+    -Wno-narrowing
+    -Wno-deprecated-register)
+
+target_compile_definitions(trinity-compile-option-interface
+  INTERFACE
+    -DDEBUG=1)
 
--- cmake/compiler/gcc/settings.cmake
+++ cmake/compiler/gcc/settings.cmake
@@ -1,30 +1,46 @@
 # Set build-directive (used in core to tell which buildtype we used)
-add_definitions(-D_BUILD_DIRECTIVE='"${CMAKE_BUILD_TYPE}"')
+target_compile_definitions(trinity-compile-option-interface
+  INTERFACE
+    -D_BUILD_DIRECTIVE="${CMAKE_BUILD_TYPE}")
 
-add_definitions(-fno-delete-null-pointer-checks)
+set(GCC_EXPECTED_VERSION 6.3.0)
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-message(STATUS "GCC: Enabled c++11 support")
-
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -std=gnu99")
-message(STATUS "GCC: Enabled C99 support")
+if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS GCC_EXPECTED_VERSION)
+  message(FATAL_ERROR "GCC: MagicStormLegion requires version ${GCC_EXPECTED_VERSION} to build but found ${CMAKE_CXX_COMPILER_VERSION}")
+endif()
 
 if(PLATFORM EQUAL 32)
   # Required on 32-bit systems to enable SSE2 (standard on x64)
-  add_definitions(-msse2 -mfpmath=sse)
+  target_compile_options(trinity-compile-option-interface
+    INTERFACE
+      -msse2
+      -mfpmath=sse)
 endif()
-add_definitions(-DHAVE_SSE2 -D__SSE2__)
+target_compile_definitions(trinity-compile-option-interface
+  INTERFACE
+    -DHAVE_SSE2
+    -D__SSE2__)
+
 message(STATUS "GCC: SSE2 flags forced")
 
 if( WITH_WARNINGS )
-  add_definitions(-Wall -Wfatal-errors -Wextra)
+  target_compile_options(trinity-warning-interface
+    INTERFACE
+      -W
+      -Wall
+      -Wextra
+      -Winit-self
+      -Winvalid-pch
+      -Wfatal-errors
+      -Woverloaded-virtual)
+
   message(STATUS "GCC: All warnings enabled")
-else()
-  add_definitions(--no-warnings)
-  message(STATUS "GCC: All warnings disabled")
 endif()
 
 if( WITH_COREDEBUG )
-  add_definitions(-ggdb3)
+  target_compile_options(trinity-compile-option-interface
+    INTERFACE
+      -g3)
+
   message(STATUS "GCC: Debug-flags set (-ggdb3)")
 endif()
