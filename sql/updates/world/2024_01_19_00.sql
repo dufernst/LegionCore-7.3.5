824d1984c22e8b11862c530ff5940edfd13065c3
SoulSeekkor <soulseekkor@live.com>
2024-01-19 19:04:31 -0600

    More quest chance fixes.

--- sql/updates/world/2024_01_19_00.sql
+++ sql/updates/world/2024_01_19_00.sql
@@ -1,18 +1,21 @@
 -- Fix loot chance for the following quest items
 
 -- 2636 = Carved Stone Idol
+-- 55232 = Threshadon Chunk
 -- 57131 = Intact Crocolisk Jaw
 -- 60402 = Mosshide Ear
 -- 60404 = Foreman Sharpsneer's Head
 -- 60497 = Bear Rump
+-- 60511 = Murloc Scent Gland
 -- 60754 = Glassy Hornet Wing
 -- 60755 = Fluffy Fox Tail
 
-DELETE FROM `creature_loot_template` WHERE `item` IN (2636,57131,60402,60404,60497,60754,60755);
+DELETE FROM `creature_loot_template` WHERE `item` IN (2636,55232,57131,60402,60404,60497,60511,60754,60755);
 INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`, `shared`) VALUES
 (1165, 2636, -75, 0, 0, 1, 1, 0),
 (1167, 2636, -75, 0, 0, 1, 1, 0),
 
+(41137, 55232, -100, 0, 0, 1, 1, 0),
 (1693, 57131, -65, 0, 0, 1, 1, 0),
 
 (44161, 60402, -100, 0, 0, 1, 1, 0),
@@ -21,6 +24,10 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 
 (44198, 60404, -100, 0, 0, 1, 1, 0),
 (1186, 60497, -90, 0, 0, 1, 1, 0),
+
+(44176, 60511, -65, 0, 0, 1, 1, 0),
+(44292, 60511, -65, 0, 0, 1, 1, 0),
+(45401, 60511, -65, 0, 0, 1, 1, 0),
 
 (44620, 60754, -65, 0, 0, 1, 1, 0),
 (45402, 60754, -65, 0, 0, 1, 1, 0),
