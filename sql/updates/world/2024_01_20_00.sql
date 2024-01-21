388548cf2f9b2348d58bfbb52ae487b54923cd77
SoulSeekkor <soulseekkor@live.com>
2024-01-20 18:29:04 -0600

    More quest chance fixes.

--- sql/updates/world/2024_01_20_00.sql
+++ sql/updates/world/2024_01_20_00.sql
@@ -1,11 +1,19 @@
 -- Fix loot chance for the following quest items
 
+-- 2629 = Intrepid Strongbox Key
+-- 3183 = Mangy Claw
 -- 3618 = Gobbler's Head
 -- 4503 = Witherbark Tusk
 -- 4515 = Marez's Head
 -- 4516 = Otto's Head
 -- 4517 = Falconcrest's Head
 -- 4522 = Witherbark Medicine Pouch
+-- 23687 = Blacktalon's Claws
+-- 28513 = Demonic Rune Stone (should be negative)
+-- 29795 = Burning Legion Gate Key
+-- 30158 = Morkh's Shattered Armor
+-- 31347 = Bleeding Hollow Torch (drops when it should not, loot chance was all over with one negative and rest positive)
+-- 52305 = Humming Electrogizard
 -- 55234 = Dumpy Level
 -- 55988 = Glowerglare's Beard
 -- 56013 = Meaty Crawler Claw
@@ -16,8 +24,12 @@
 -- 58779 = Shell of Shadra
 -- 60737 = Stabthistle Seed
 
-DELETE FROM `creature_loot_template` WHERE `item` IN (3618,4503,4515,4516,4517,4522,55234,55988,56013,56083,56087,56088,56089,58779,60737);
+DELETE FROM `creature_loot_template` WHERE `item` IN (2629,3183,3618,4503,4515,4516,4517,4522,23687,28513,29795,30158,31347,52305,55234,55988,56013,56083,56087,56088,56089,58779,60737);
 INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`, `shared`) VALUES
+(1160, 2629, -100, 0, 0, 1, 1, 0),
+(41429, 2629, -100, 0, 0, 1, 1, 0),
+
+(1961, 3183, -100, 0, 0, 1, 1, 0),
 (1259, 3618, -100, 0, 0, 1, 1, 0),
 
 (2552, 4503, -40, 0, 0, 1, 1, 0),
@@ -38,6 +50,23 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 (2555, 4522, -40, 0, 0, 1, 1, 0),
 (51633, 4522, -40, 0, 0, 1, 1, 0),
 
+(17057, 23687, -100, 0, 0, 1, 1, 0),
+
+(16950, 28513, -60, 0, 0, 1, 1, 0),
+(18975, 28513, -85, 0, 0, 1, 1, 0),
+(18981, 28513, -60, 0, 0, 1, 1, 0),
+(19190, 28513, -85, 0, 0, 1, 1, 0),
+(19282, 28513, -25, 0, 0, 1, 1, 0),
+
+(16946, 29795, -0.1699, 0, 0, 1, 1, 0),
+(16947, 29795, -0.0916, 0, 0, 1, 1, 0),
+(16954, 29795, -0.0267, 0, 0, 1, 1, 0),
+(16960, 29795, -0.0187, 0, 0, 1, 1, 0),
+(19298, 29795, -100, 0, 0, 1, 1, 0),
+
+(16964, 30158, -100, 0, 0, 1, 1, 0),
+(16907, 31347, -50, 0, 0, 1, 1, 0),
+(37208, 52305, -25, 0, 0, 1, 1, 0),
 (41145, 55234, -15, 0, 0, 1, 1, 0),
 
 (41295, 56013, -50, 0, 0, 1, 1, 0),
