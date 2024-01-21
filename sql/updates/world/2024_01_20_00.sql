83949e6e5dc6bdd588220a92dec464c3f8f3cb0d
SoulSeekkor <soulseekkor@live.com>
2024-01-20 18:06:35 -0600

    Fix "Young Murk Thresher" drop table in order to fix broken quest drops for it.

--- sql/updates/world/2024_01_20_00.sql
+++ sql/updates/world/2024_01_20_00.sql
@@ -6,7 +6,6 @@
 -- 4516 = Otto's Head
 -- 4517 = Falconcrest's Head
 -- 4522 = Witherbark Medicine Pouch
--- 33126 = Thresher Oil
 -- 55234 = Dumpy Level
 -- 55988 = Glowerglare's Beard
 -- 56013 = Meaty Crawler Claw
@@ -17,7 +16,7 @@
 -- 58779 = Shell of Shadra
 -- 60737 = Stabthistle Seed
 
-DELETE FROM `creature_loot_template` WHERE `item` IN (3618,4503,4515,4516,4517,4522,33126,55234,55988,56013,56083,56087,56088,56089,58779,60737);
+DELETE FROM `creature_loot_template` WHERE `item` IN (3618,4503,4515,4516,4517,4522,55234,55988,56013,56083,56087,56088,56089,58779,60737);
 INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`, `shared`) VALUES
 (1259, 3618, -100, 0, 0, 1, 1, 0),
 
@@ -39,7 +38,6 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 (2555, 4522, -40, 0, 0, 1, 1, 0),
 (51633, 4522, -40, 0, 0, 1, 1, 0),
 
-(4388, 33126, -75, 0, 0, 1, 1, 0),
 (41145, 55234, -15, 0, 0, 1, 1, 0),
 
 (41295, 56013, -50, 0, 0, 1, 1, 0),
@@ -59,3 +57,22 @@ INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `l
 
 (44635, 60737, -2, 0, 0, 1, 1, 0),
 (44638, 60737, -2, 0, 0, 1, 1, 0);
+
+-- Fix "Young Murk Thresher" drop table (which will fix the "Thresher Oil" quest drop)
+
+-- NOTE: This is for CREATURE, not ITEM!
+DELETE FROM `creature_loot_template` WHERE `entry` IN (4388);
+INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `lootmode`, `groupid`, `mincountOrRef`, `maxcount`, `shared`) VALUES
+(4388, 1529, 0.06, 0, 0, 1, 1, 0),
+(4388, 1705, 0.03, 0, 0, 1, 1, 0),
+(4388, 1725, 0.03, 0, 0, 1, 1, 0),
+(4388, 2608, 10, 0, 0, 1, 1, 0),
+(4388, 3864, 0.03, 0, 0, 1, 1, 0),
+(4388, 4634, 0.1, 0, 0, 1, 1, 0),
+(4388, 4636, 0.1, 0, 0, 1, 1, 0),
+(4388, 5516, 22, 0, 0, 1, 1, 0),
+(4388, 5637, 6, 0, 0, 1, 1, 0),
+(4388, 6362, 15, 0, 0, 1, 2, 0),
+(4388, 7084, 0.03, 0, 0, 1, 1, 0),
+(4388, 7973, 45, 0, 0, 1, 1, 0),
+(4388, 33126, -60, 0, 0, 1, 1, 0); -- Thresher Oil
