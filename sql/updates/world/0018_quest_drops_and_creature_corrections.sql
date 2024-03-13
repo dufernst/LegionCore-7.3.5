25d49c74f8cb33e5e458c00b8ea034624ec4b8c5
SoulSeekkor <soulseekkor@live.com>
2024-03-12 20:19:06 -0500

    Fix broken Wooly Mammoth Bull creature.

--- sql/updates/world/0018_quest_drops_and_creature_corrections.sql
+++ sql/updates/world/0018_quest_drops_and_creature_corrections.sql
@@ -1294,3 +1294,18 @@ UPDATE `quest_template` SET `LogDescription` = 'Well hello there, young $C. Eith
 -- 11672 = Enlistment Day
 
 UPDATE `quest_template_addon` SET `PrevQuestID` = 0 WHERE `ID` = 11672;
+UPDATE `quest_template_addon` SET `NextQuestID` = 0 WHERE `ID` = 28709;  -- Hero's Call: Borean Tundra! (should NOT lead into the other quest)
+
+-- Fix broken creatures
+
+-- 24614 = Wooly Mammoth
+-- 25743 = Wooly Mammoth Bull (TONS wrong with it and it one shots you)
+
+UPDATE `creature_template` SET `npcflag` = 16777216, `unit_flags2` = 2048 WHERE `entry` = 25743;
+UPDATE `creature_template` SET `dynamicflags` = 0 WHERE `entry` = 24614;
+
+DELETE FROM `smart_scripts` WHERE `entryorguid` = 25743;
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES
+(25743,0,0,0,6,0,100,512,0,0,0,0,11,46221,2,0,0,0,0,7,0,0,0,0,0,0,0,'Wooly Mammoth Bull - On Just Died - Cast ''Animal Blood'''),
+(25743,0,1,0,27,0,100,0,0,0,0,0,8,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Wooly Mammoth Bull - On Passenger Boarded - Set ReactState Passive'),
+(25743,0,2,0,28,0,100,0,0,0,0,0,41,1000,0,0,0,0,0,1,0,0,0,0,0,0,0,'Wooly Mammoth Bull - On Passenger Removed - Despawn');
